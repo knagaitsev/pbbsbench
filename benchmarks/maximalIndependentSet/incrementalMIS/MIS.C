cfad7c104d3964c600cb6501ba34239754112233
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2025-02-04 03:38:12 +0000

    add parallel luby implementation

--- benchmarks/maximalIndependentSet/incrementalMIS/MIS.C
+++ benchmarks/maximalIndependentSet/incrementalMIS/MIS.C
@@ -21,49 +21,132 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <iostream>
-#include "parlay/parallel.h"
 #include "parlay/primitives.h"
+#include "parlay/random.h"
 #include "common/graph.h"
-#include "common/speculative_for.h"
-#include "common/get_time.h"
 #include "MIS.h"
-using namespace std;
+
+template<typename T>
+  parlay::sequence<T> rand(size_t s, size_t e) {
+    parlay::random_generator gen(0);
+    std::uniform_real_distribution<double> dis(0.0,1.0);
+    auto In = parlay::tabulate(e-s, [&] (size_t i) -> T{
+      auto r = gen[i+s];
+      return dis(r);});
+    return In;
+  }
 
 // **************************************************************
 //    MAXIMAL INDEPENDENT SET
 // **************************************************************
 
-// For each vertex:
-//   Flags = 0 indicates undecided
-//   Flags = 1 indicates chosen
-//   Flags = 2 indicates a neighbor is chosen
-struct MISstep {
-  char flag;
-  parlay::sequence<char> &Flags;
-  Graph const &G;
-  MISstep(parlay::sequence<char> & F, const Graph &G) : Flags(F), G(G) {}
-
-  bool reserve(size_t i) {
-    size_t d = G[i].degree;
-    flag = 1;
-    for (size_t j = 0; j < d; j++) {
-      size_t ngh = G[i].Neighbors[j];
-      if (ngh < i) {
-	if (Flags[ngh] == 1) { flag = 2; return 1;}
-	// need to wait for higher priority neighbor to decide
-	else if (Flags[ngh] == 0) flag = 0; 
-      }
-    }
-    return 1;
-  }
-
-  bool commit(size_t i) { return (Flags[i] = flag) > 0;}
-};
-
-parlay::sequence<char> maximalIndependentSet(Graph const &GS) {
-  size_t n = GS.n;
+parlay::sequence<char> maximalIndependentSet(Graph const &G)
+{
+  size_t n = G.n;
   parlay::sequence<char> Flags(n, (char) 0);
-  MISstep mis(Flags, GS);
-  pbbs::speculative_for<vertexId>(mis, 0, n, 20);
+
+  // auto G = graph<uint, uint>(origGraph.offsets, origGraph.edges, n);
+
+  // parlay::sequence<uint> rands(n, 0);
+
+  parlay::sequence<bool> toRemove(n, false);
+  parlay::sequence<bool> removed(n, false);
+  parlay::sequence<int> activeFlags(n, 0);
+  parlay::sequence<double> ranks(n, 0.0);
+
+  parlay::random_generator gen(0);
+  std::uniform_real_distribution<double> dis(0.0, 1.0);
+
+  // int iter = 0;
+
+  while (true) {
+    // std::cout << "Iter: " << iter << "\n";
+    // iter++;
+
+    // for (size_t i = 0; i < n; i++) {
+    //   rands[i] = dis(gen);
+    // }
+
+    bool allRemoved = true;
+    parlay::parallel_for(0, n, [&] (size_t i) {
+    // for (size_t i = 0; i < n; i++) {
+      removed[i] = toRemove[i];
+      if (!toRemove[i]) {
+        allRemoved = false;
+
+        // auto rand = rands[i];
+        auto rand = dis(gen);
+
+        uint deg = 0;
+        for (size_t j = 0; j < G[i].degree; j++) {
+          vertexId ngh = G[i].Neighbors[j];
+          deg += (uint)(!toRemove[ngh]);
+        }
+
+        int activeFlag = deg * rand < 0.5;
+        // int activeFlag = rand < 0.5;
+        activeFlags[i] = activeFlag;
+
+        double rank = (double)activeFlag * ((double)(deg) + (1.0)/((double)(i + 1)));
+        ranks[i] = rank;
+      }
+    });
+
+    if (allRemoved) {
+      break;
+    }
+
+    parlay::parallel_for(0, n, [&] (size_t i) {
+    // for (size_t i = 0; i < n; i++) {
+      if (!removed[i] && activeFlags[i]) {
+        double myRank = ranks[i];
+
+        bool good = true;
+        for (size_t j = 0; j < G[i].degree; j++) {
+          vertexId ngh = G[i].Neighbors[j];
+          if (!removed[ngh] && myRank <= ranks[ngh]) {
+            good = false;
+            break;
+          }
+        }
+
+        if (good) {
+          Flags[i] = 1;
+          // removed[i] = true;
+          toRemove[i] = true;
+          for (size_t j = 0; j < G[i].degree; j++) {
+            vertexId ngh = G[i].Neighbors[j];
+            Flags[ngh] = 2;
+            // removed[ngh] = true;
+            toRemove[ngh] = true;
+          }
+        }
+      }
+    });
+
+    // parlay::parallel_for(0, n, [&] (size_t i) {
+    // // for (size_t i = 0; i < n; i++) {
+    //   removed[i] = toRemove[i];
+    // });
+  }
+  // for (size_t i = 0; i < n; i++) {
+  //   Flags[i] = 1;
+  //   for (size_t j = 0; j< G[i].degree; j++) {
+  //     vertexId ngh = G[i].Neighbors[j];
+  //     if (Flags[ngh] == 1) {
+	// Flags[i] = 2;
+	// break;
+  //     }
+  //   }
+  // }
+
+  // int c = 0;
+  // for (size_t i = 0; i < n; i++) {
+  //   if (Flags[i] == 1) {
+  //     c++;
+  //   }
+  // }
+  // std::cout << "Flags len: " << c << "\n";
+
   return Flags;
 }
